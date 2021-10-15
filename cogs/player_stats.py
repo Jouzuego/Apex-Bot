535ea975fc4067f1ab00c517eb9e666600b51a64
Josh <sparkplug282@gmail.com>
2021-10-14 20:30:22 -0500

    updated player stats command. created legend stat command

--- cogs/player_stats.py
+++ cogs/player_stats.py
@@ -14,12 +14,32 @@ class player_stats(commands.Cog):
         self.client = client
 
     @commands.command(aliases = ['ps'])
-    async def player_stats(self, ctx, arg1, arg2):
+    async def player_stats(self, ctx, arg1):
         response = requests.get('https://api.mozambiquehe.re/bridge?version=5&platform=PC&player={}&auth={}' .format(arg1, TOKEN))
         data = response.json()
         #print(json.dumps(data, sort_keys=False, indent=4))
-        legend_stats = data['legends']['all'][arg2]['data']
-        await ctx.send(legend_stats)
+        print(json.dumps(data['total'], sort_keys=False, indent=4))
+        player_kills = data['total']['kills']['value']
+        player_kd = data['total']['kd']['value']
+        player_rank_name = data['global']['rank']['rankName']
+        player_rank_div = data['global']['rank']['rankDiv']
+        
+        await ctx.send("Total kills: {}.  KD: {}. rank: {} {}" .format(player_kills, player_kd, player_rank_name, player_rank_div))
+
+    @commands.command(aliases = ['ls'])
+    async def legend_stats(self, ctx, arg1, arg2):
+        response = requests.get('https://api.mozambiquehe.re/bridge?version=5&platform=PC&player={}&auth={}' .format(arg1, TOKEN))
+        data = response.json()
+        print(json.dumps(data['legends']['all'][arg2], sort_keys=False, indent=4))
+        legend_icon = data['legends']['all'][arg2]['ImgAssets']['icon']
+        await ctx.send("{}" .format(legend_icon))
+        for tracker in data["legends"]["all"][arg2]["data"]:
+            legend_tracker_name = tracker['name']
+            legend_tracker_value =tracker['value']
+            await ctx.send("{}: {}" .format(legend_tracker_name, legend_tracker_value))
+
+    
+
 
 
 def setup(client):
